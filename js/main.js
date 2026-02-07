e0a8f83d4253d642638533f8fa1c9c8faebc469d
Иван Чесноков <gr434_cheial@lab-211-3.tomtit-tomsk.ru>
2026-02-07 09:54:28 +0700

    1 practic

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        description: "A pair of warm, fuzzy socks.",
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg"
+    }
+})
+
