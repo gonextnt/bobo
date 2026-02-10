3f32934445fd6dfcb670eb9e0e85877ec94f1057
Иван Чесноков <gr434_cheial@lab-203-1.tomtit-tomsk.ru>
2026-02-10 10:50:56 +0700

    5 practik

--- js/main.js
+++ js/main.js
@@ -10,15 +10,37 @@ let app = new Vue({
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+
+
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ]
-    }
+
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        delToCart() {
+            if (this.cart > 0) {
+                this.cart -= 1;
+            }
+
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+
+    },
+
 })
