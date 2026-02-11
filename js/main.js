2bd1608838e0a64a0fe0bad09ff973bef89e27c9
Иван Чесноков <gr434_cheial@lab-203-1.tomtit-tomsk.ru>
2026-02-11 13:37:49 +0700

    Danil Kolbasenko

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         description: "A pair of warm, fuzzy socks",
         altText: "A pair of socks",
         link:"https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -18,11 +18,13 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ]
 
@@ -37,10 +39,30 @@ let app = new Vue({
             }
 
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         },
 
+
+
     },
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale(){
+            if (this.onSale === true)
+                    return this.brand + ' сдается в аренду ' + this.product + ' со скидкой 50%!';
+        },
+    },
+
 
 })
